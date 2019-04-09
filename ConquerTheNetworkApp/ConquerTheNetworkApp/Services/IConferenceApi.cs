8f8849cd4b6b93b539a090b38be25ade8e95b757
Roy Cornelissen <roy_cornelissen@hotmail.com>
2019-04-09 11:04:44 +0200

    Use Refit instead of boilerplate HttpClient

---
+++ ConquerTheNetworkApp/ConquerTheNetworkApp/Services/IConferenceApi.cs
@@ -0,0 +1,20 @@
+﻿using ConquerTheNetworkApp.Data;
+using Refit;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConquerTheNetworkApp.Services
+{
+	[Headers("Accept: application/json")]
+	public interface IConferenceApi
+	{
+		[Get("/api/cities")]
+		Task<List<City>> GetCities();
+
+		[Get("/api/schedule/{id}")]
+		Task<Schedule> GetScheduleForCity(string id);
+
+		[Post("/api/rating")]
+		Task Rate([Body] double rating);
+	}
+}
--- ConquerTheNetworkApp/ConquerTheNetworkApp/Services/ServiceClient.cs
+++ ConquerTheNetworkApp/ConquerTheNetworkApp/Services/ServiceClient.cs
@@ -1,86 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using Newtonsoft.Json;
 using ConquerTheNetworkApp.Data;
+using Refit;
 
 namespace ConquerTheNetworkApp.Services
 {
-    public class ServiceClient
-    {
-        public static string ApiBaseAddress = "http://conquerthenetworksampleservice.azurewebsites.net";
+	public class ServiceClient
+	{
+		public static string ApiBaseAddress = "http://conquerthenetworksampleservice.azurewebsites.net";
 
-        public async Task<List<City>> GetCities()
-        {
-            IEnumerable<City> cities = new List<City>();
+		private IConferenceApi _client;
 
-            using (var httpClient = CreateClient())
-            {
-                var response = await httpClient.GetAsync("api/cities").ConfigureAwait(false);
-                if (response.IsSuccessStatusCode)
-                {
-                    var json = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                    if (!string.IsNullOrWhiteSpace(json))
-                    {
-                        cities = await Task.Run(() =>
-                            JsonConvert.DeserializeObject<IEnumerable<City>>(json)
-                            ).ConfigureAwait(false);
+		public ServiceClient()
+		{
+			_client = RestService.For<IConferenceApi>(ApiBaseAddress);
+		}
 
-                    }
-                }
-            }
+		public Task<List<City>> GetCities()
+		{
+			return _client.GetCities();
+		}
 
-            return cities.ToList();
-        }
+		public async Task<Schedule> GetScheduleForCity(string id)
+		{
+			try
+			{
+				return await _client.GetScheduleForCity(id);
+			}
+			catch (ApiException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+			{
+				return null;
+			}
+		}
 
-        public async Task<Schedule> GetScheduleForCity(string id)
-        {
-            using (var httpClient = CreateClient())
-            {
-                var response = await httpClient.GetAsync($"api/schedule/{id}").ConfigureAwait(false);
-                if (response.IsSuccessStatusCode)
-                {
-                    var json = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                    if (!string.IsNullOrWhiteSpace(json))
-                    {
-                        var schedule = await Task.Run(() =>
-                            JsonConvert.DeserializeObject<Schedule>(json)
-                            ).ConfigureAwait(false);
-
-                        return schedule;
-                    }
-                }
-            }
-            return null;
-        }
-
-        public async Task SendRating(double rating)
-        {
-            using (var httpClient = CreateClient())
-            {
-                var message = new HttpRequestMessage(HttpMethod.Post, $"api/rating");
-                var response = await httpClient.SendAsync(message).ConfigureAwait(false);
-                if (!response.IsSuccessStatusCode)
-                {
-                    throw new Exception("Whoops!");
-                }
-            }
-        }
-
-        private HttpClient CreateClient()
-        {
-            var httpClient = new HttpClient
-            {
-                BaseAddress = new Uri(ApiBaseAddress)
-            };
-
-            httpClient.DefaultRequestHeaders.Accept.Clear();
-            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            return httpClient;
-        }
-    }
+		public Task SendRating(double rating)
+		{
+			return _client.Rate(rating);
+		}
+	}
 }
